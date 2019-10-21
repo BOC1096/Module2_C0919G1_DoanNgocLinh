a27143a3941385f720ebcfa89e7ea8ac9f2058f8
BOC1096 <linh000boc@gmail.com>
2019-10-21 11:06:24 +0700

    sua loi bai ke thua

--- src/B4_KeThua/BT_B2LopPoint2DvaLopPoint3D/PointTest.java
+++ src/B4_KeThua/BT_B2LopPoint2DvaLopPoint3D/PointTest.java
@@ -3,7 +3,7 @@ package B4_KeThua.BT_B2LopPoint2DvaLopPoint3D;
 public class PointTest {
     public static void main(String[] args) {
         Ponint3D ponint3D = new Ponint3D(3, 5, 7);
-        ponint3D.setXYZ(ponint3D.xyz);
+        ponint3D.setXYZ(ponint3D.getXYZ());
         System.out.println(ponint3D.toString());
     }
 }
